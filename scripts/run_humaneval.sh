c271c7eb2a6518fbfa2635acd46a1865992fc6f7
0324wy <0324wy@gmail.com>
2024-10-25 18:07:56 +0000

    run script

---
+++ scripts/run_humaneval.sh
@@ -0,0 +1,3 @@
+python ../src/programmer_humaneval.py
+python ../src/test_designer_humaneval.py
+python ../src/test_executor_humaneval.py
---
+++ scripts/run_mbpp.sh
@@ -0,0 +1,3 @@
+python ../src/programmer_mbpp.py
+python ../src/test_designer_mbpp.py
+python ../src/test_executor_mbpp.py
